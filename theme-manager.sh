5393fc7368ffe705ed551158bbae8d7085687d63
Heliwrenaid <soulh24xda@gmail.com>
2023-07-18 16:38:36 +0200

    improved theme-manager.sh

--- theme-manager.sh
+++ theme-manager.sh
@@ -1,15 +1,40 @@
 #!/bin/bash
 
 SCRIPT_DIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
-BACKUP_FILEPATH="$HOME/theme-manager-dotfiles-copy.tar.gz"
 CONFIG_DIR="$HOME/.config"
 ALL_DIRS=(eww hypr rofi scripts dunst kitty)
 
-install() {
-    mkdir -p "$CONFIG_DIR"
+THEME_MANAGER_DIR="$HOME/.theme-manager"
+BACKUP_FILENAME="$THEME_MANAGER_DIR/dotfiles-copy"
+
+get_new_backup_filepath() {
+    echo "${BACKUP_FILENAME}-v$(get_new_backup_version).tar.gz"
+}
+
+get_new_backup_version() {
+    declare -i i=1
+    declare backup_file="${BACKUP_FILENAME}-v${i}.tar.gz"
+    while [ -f $backup_file ]; do
+        i=$((i+1))
+        backup_file="${BACKUP_FILENAME}-v${i}.tar.gz"
+    done
+    echo $i
+}
+
+get_last_backup_version() {
+    declare -i version=`get_new_backup_version`
+    version=$((version-1))
+    echo $version
+}
+
+get_backup_filepath() {
+    echo "${BACKUP_FILENAME}-v${1}.tar.gz"
+}
+
+create_backup() {
     cd "$CONFIG_DIR"
 
-    selected_dirs=()
+    declare -a selected_dirs
     for dir in ${ALL_DIRS[@]}; do
         if [ -d $dir ]; then
             selected_dirs+=( $dir )
@@ -17,43 +42,68 @@ install() {
     done
 
     if [ ${#selected_dirs[@]} -ne 0 ]; then
-        if [ ! -f "$BACKUP_FILEPATH" ]; then
-            tar -czf "$BACKUP_FILEPATH" ${selected_dirs[@]} -h
-            if [ $? -eq 0 ]; then
-                echo "Created current dotfiles copy: $BACKUP_FILEPATH"
-            else
-                echo "Cannot create archive"
-                exit 1
-            fi
+        declare backup_file=`get_new_backup_filepath`
+        tar -czf "$backup_file" ${selected_dirs[@]} -h
+        if [ $? -eq 0 ]; then
+            echo "Created backup of dotfiles: $backup_file"
+        else
+            echo "Operation failed. Cannot create backup"
+            exit 1
         fi
-        rm -rf ${selected_dirs[@]}
+    else 
+        echo "There aren't any directories conflicts. Skipping backup creation"
     fi
+}
 
+install() {
+    create_backup
+    rm -rf ${ALL_DIRS[@]}
     cd "$SCRIPT_DIR/.config"
     stow . -t "$CONFIG_DIR"
-}
-
-uninstall() {
-    cd "$CONFIG_DIR"
-    if [ ! -f "$BACKUP_FILEPATH" ]; then
-        echo "Operation failed. Backup file doesn't exists: $BACKUP_FILEPATH"
+    if [ $? -eq 0 ]; then
+        echo "Successfuly installed theme"
+    else
+        echo "Operation failed. Cannot create symlinks"
         exit 1
     fi
+}
+
+rollback() {
+    declare -i last_version=`get_last_backup_version`
+    if [ $last_version -eq 0 ]; then
+        echo "Operation failed. There is no backups in: $THEME_MANAGER_DIR"
+        exit 1
+    elif [ $last_version -eq 1 ]; then
+        echo "Existing backup versions: 1"
+    else
+        echo "Existing backup versions: [1 - OLDEST, $last_version - NEWEST]"
+    fi
+
+    read -p "Choose version: " version
+    backup_file=`get_backup_filepath $version`
+    if [ ! -f $backup_file ]; then
+        echo "Operation failed. Backup file: $backup_file doesn't exist"
+        exit 1
+    fi
+
+    cd "$CONFIG_DIR"
+    
     rm -rf ${ALL_DIRS[@]}
-    tar -xzf "$BACKUP_FILEPATH" ${selected_dirs[@]} -h
-    if [ $? -eq 0 ]; then
-        rm -f "$BACKUP_FILEPATH"
+    tar -xzf "$backup_file" -h
+    if [ $? -ne 0 ]; then
+        echo "Operation failed. Cannot unpack backup"
+        exit 1
     fi
 }
+
+mkdir -p "$CONFIG_DIR"
+mkdir -p "$THEME_MANAGER_DIR"
 
 case "$1" in
     install) install ;;
-    uninstall) uninstall ;;
-    *) echo "Use one of commands: install, uninstall" ;;
+    rollback) rollback ;;
+    delete-backups) rm -rf $THEME_MANAGER_DIR ;;
+    create-backup) create_backup ;;
+    list-backups) ls -l $THEME_MANAGER_DIR | grep .tar.gz | cut -d " " -f6-9 ;;
+    *) echo "Use one of commands: install, rollback, create-backup, delete-backups, list-backups" && exit 0 ;;
 esac
-
-if [ $? -eq 0 ]; then
-    echo "Operation successful"
-else
-    echo "Operation failed"
-fi
